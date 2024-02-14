e9125a4969387cbb2296eb0e64a367d3d1cf3117
ezbishal <ezbishal@gmail.com>
2024-02-14 19:28:32 +0000

    Add copilot.vim plugin and update key mappings

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -24,12 +24,14 @@ vim.keymap.set("n", "L", ":bnext<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>bd", ":bnext<CR>:bd#<CR>", { noremap = true, silent = true })
 --
 
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("i", "kj", "<Esc>", { noremap = true, silent = true })
 vim.keymap.set("i", "jj", "<C-o>", { noremap = true, silent = true })
 
-vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>")
+vim.keymap.set("n", "<leader>/", ":nohlsearch<CR>")
 vim.keymap.set({"n","v"}, "J", "5j", { noremap = true, silent = true })
 vim.keymap.set({"n","v"}, "K", "5k", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>j", "J", { noremap = true, silent = true })
