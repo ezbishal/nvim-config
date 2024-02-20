5e23a92be8539d87d1b7b74fa2292a0ff5e6df2d
ezbishal <ezbishal@gmail.com>
2024-02-20 21:10:44 +0000

    Changed keymaps

--- lua/options.lua
+++ lua/options.lua
@@ -2,24 +2,15 @@ vim.wo.number = true
 vim.g.mapleader = " "
 vim.opt.clipboard:append("unnamedplus")
 
--- Panes
-vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
-vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
-vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
-vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
-
 vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { noremap = true, silent = true })
---
 
--- Buffers
-vim.keymap.set("n", "H", ":bprevious<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "L", ":bnext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-h>", ":bprevious<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-l>", ":bnext<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>bd", ":bnext<CR>:bd#<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>bp", ":BufferLineTogglePin<CR>", { noremap = true, silent = true })
---
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
@@ -31,5 +22,5 @@ vim.keymap.set("n", "<leader>/", ":nohlsearch<CR>")
 vim.keymap.set({"n","v"}, "J", "5j", { noremap = true, silent = true })
 vim.keymap.set({"n","v"}, "K", "5k", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>j", "J", { noremap = true, silent = true })
-vim.keymap.set("n", "tb", "^", { noremap = true, silent = true })
-vim.keymap.set("n", "te", "g_", { noremap = true, silent = true })
+vim.keymap.set("n", "H", "^", { noremap = true, silent = true })
+vim.keymap.set("n", "L", "g_", { noremap = true, silent = true })
