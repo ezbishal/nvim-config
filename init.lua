c0baaf69aa5650fde80b8e9436e4f4df1c51ff18
ezbishal <ezbishal@gmail.com>
2024-03-13 19:15:17 +0000

    Changes

--- init.lua
+++ init.lua
@@ -12,5 +12,7 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
+vim.api.nvim_set_hl(0, 'NonText', {ctermbg = 'none'})
+
 require("options")
 require("lazy").setup("plugins")
--- lua/options.lua
+++ lua/options.lua
@@ -1,26 +1,21 @@
 vim.wo.number = true
 vim.g.mapleader = " "
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set noswapfile")
+vim.cmd("set undofile")
 vim.opt.clipboard:append("unnamedplus")
-
-vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<C-h>", ":bprevious<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-l>", ":bnext<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>bd", ":bnext<CR>:bd#<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>bp", ":BufferLineTogglePin<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>bo", ":BufferLineCloseOthers<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>w", ":w<CR>", { noremap = true, silent = true })
-
 vim.keymap.set("i", "kj", "<Esc>", { noremap = true, silent = true })
-
 vim.keymap.set("n", "<leader>/", ":nohlsearch<CR>")
-vim.keymap.set({"n","v"}, "J", "5j", { noremap = true, silent = true })
-vim.keymap.set({"n","v"}, "K", "5k", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>j", "J", { noremap = true, silent = true })
-vim.keymap.set("n", "H", "^", { noremap = true, silent = true })
-vim.keymap.set("n", "L", "g_", { noremap = true, silent = true })
