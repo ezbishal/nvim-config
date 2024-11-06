76d83c621ba4549cc36fdacce99995c282e67443
ezbishal <ezbishal@gmail.com>
2024-11-06 00:06:46 +0000

    Resolve shell dynamically for toggle-term

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -1,11 +1,14 @@
 return {
 	"akinsho/toggleterm.nvim",
-  opts = {
+	opts = function()
+		local shell = vim.loop.os_uname().sysname == "Windows_NT" and "pwsh" or "bash"
+		return {
 			open_mapping = [[<c-j>]],
 			shade_terminals = true,
 			start_in_insert = true,
 			direction = "horizontal",
 			close_on_exit = true,
-			shell = "pwsh",
+			shell = shell,
 		}
+	end
 }
